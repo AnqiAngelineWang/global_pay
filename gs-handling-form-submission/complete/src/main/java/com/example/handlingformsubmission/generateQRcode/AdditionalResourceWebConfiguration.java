00ef707ab3d16867676bcfa96b7d478e0a6e5ece
AnqiAngelineWang <32228475+AnqiAngelineWang@users.noreply.github.com>
2024-12-18 22:08:02 -0500

    Delete gs-handling-form-submission directory

--- gs-handling-form-submission/complete/src/main/java/com/example/handlingformsubmission/generateQRcode/AdditionalResourceWebConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.example.handlingformsubmission.generateQRcode;
-@Configuration
-public class AdditionalResourceWebConfiguration  implements WebMvcConfigurer {
-
-        @Override
-        public void addResourceHandlers(final ResourceHandlerRegistry registry) {
-            registry.addResourceHandler("/upload/**").addResourceLocations("file://" + System.getProperty("user.dir") + "/src/main/upload/");
-        }
-    }
